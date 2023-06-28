fdb817761fd655aaf32799daf7f286af6ab6fbfa
RiverNotHere <gyoushijie@gmail.com>
2023-06-28 13:08:14 -0400

    Added chapter 1 folder for usaco training site

---
+++ training/ch1/ride.java
@@ -0,0 +1,35 @@
+/*
+ID: gyoushi1
+LANG: JAVA
+TASK: ride
+*/
+
+import java.util.*;
+import java.io.*;
+
+public class ride {
+  public static void main(String[] args) throws IOException {
+    BufferedReader f = new BufferedReader(new FileReader("ride.in"));
+    PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("ride.out")));
+    String comet, group;
+    
+    comet = f.readLine();
+    int cometTotal = 1;
+    for (Character c : comet.toCharArray()) {
+      if (c != ' ') cometTotal *= c - 'A' + 1;
+    }
+    cometTotal %= 47;
+
+    group = f.readLine();
+    int groupTotal = 1;
+    for (Character c : group.toCharArray()) {
+      if (c != ' ') groupTotal *= c - 'A' + 1;
+    }
+    groupTotal %= 47;
+
+    if (cometTotal == groupTotal) out.println("GO");
+    else out.println("STAY");
+    
+    out.close();
+  }
+}
